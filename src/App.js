b44dfd306dbb661ec7300bd1d909fc93fd82f592
Carlos Valter dos Santos Ferreira <carlosvalter@hotmail.com>
2020-04-29 22:12:27 -0300

    Front-end connect in api back-end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  // Executa sempre q o componente for exibido
+  useEffect(() => { // Não é possível usar async await, por isso .then
+    api.get('repositories')
+      .then(response => {
+        setRepositories(response.data)
+      })
+  }, []);
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo ${Date.now()}`,
+      url: "http://github.com",
+      techs: "techs"
+    });
+
+    // Atualiza o estado do array
+    setRepositories([...repositories, response.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    // Encontra o index do elemento no array
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    // Remove o item do array
+    repositories.splice(repositoryIndex, 1);
+    // Atualiza o estado do array
+    setRepositories([...repositories]); // ...copia todo o conteudo de repositories para a nova []
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories.map(repository =>
+          <li key={repository.id}>{repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
